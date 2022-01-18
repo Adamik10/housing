646ca80e2e0f7eafb55dea4343cfbb3004a31417
Adam Antal <adoantal@gmail.com>
2022-01-18 12:18:06 +0100

    updated the structure of the app

---
+++ client/src/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// const properties = document.querySelectorAll(".property");
+// console.log(properties);
+
+//ReactDOM.render(
+//  <h1>Hej fra React!</h1>,
+//  document.getElementById('root')
+//);
