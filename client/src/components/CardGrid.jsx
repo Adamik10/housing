dcdbb7233aad88a8c6d8ae1be0503d469e22dc92
Adam Antal <adoantal@gmail.com>
2022-01-22 12:01:05 +0100

    documented and decentralized the code

--- client/src/components/CardGrid.jsx
+++ client/src/components/CardGrid.jsx
@@ -2,18 +2,21 @@ import React, { useEffect, useState } from "react";
 import numeral from "numeral";
 
 const CardGrid = ({ sortField, filterField }) => {
-  const [allProperties, setAllProperties] = useState({
-    data: [],
-    detailsLoaded: false,
-  });
-  const [isLoading, setIsLoading] = useState(true);
+  // allProperties state holds all the data once loaded
+  const [allProperties, setAllProperties] = useState([]);
+  // state for loading - based on this we render the content or loading
+  const [isLoading, setIsLoading] = useState(false);
+  // state for errors - based on this we render the content or error
   const [isError, setIsError] = useState(false);
 
+  // retrieves data from the API. Ideally, this would maybe be moved to its
+  // own separate component, so it can be reused, but in this project I just include it here
   const fetchData = () => {
+    setIsLoading(true);
     fetch("http://localhost:3000/api/properties")
       .then((response) => response.json())
       .then((data) => {
-        setAllProperties({ data: data, detailsLoaded: true });
+        setAllProperties(data);
         setIsLoading(false);
       })
       .catch((error) => {
@@ -23,26 +26,22 @@ const CardGrid = ({ sortField, filterField }) => {
       });
   };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  if (isLoading) {
-    return <div>Loading</div>;
-  } else {
-    let filteredSortedData = allProperties.data;
+  // function used for filtering properties
+  filterProperties = () => {
     if (filterField.min !== null) {
-      filteredSortedData = filteredSortedData.filter((value) => {
-        return value.size >= filterField.min;
-      });
+      filteredSortedData = filteredSortedData.filter(
+        (value) => value.size >= filterField.min
+      );
     }
-
     if (filterField.max !== null) {
-      filteredSortedData = filteredSortedData.filter((value) => {
-        return value.size <= filterField.max;
-      });
+      filteredSortedData = filteredSortedData.filter(
+        (value) => value.size <= filterField.max
+      );
     }
+  };
 
+  // function used for sorting properties
+  const sortProperties = () => {
     if (sortField !== null) {
       sortField === "ascending"
         ? (filteredSortedData = filteredSortedData.sort((a, b) => {
@@ -52,32 +51,54 @@ const CardGrid = ({ sortField, filterField }) => {
             return a.price - b.price;
           }));
     }
+  };
 
-    return (
-      <>
-        {filteredSortedData.map((item, index) => (
-          <div className="property" key={index}>
-            <img className="property__image" src={item.img} alt={item.title} />
-            <h2 className="property__title">{item.title}</h2>
-            <span className="property__address">
-              {item.address.street}, {item.address.number}, {item.address.zip},{" "}
-              {item.address.city}
-            </span>
-            <ul className="property__info">
-              <li className="property__price">
-                {numeral(item.price).format("0,0")},-
-              </li>
-              <li className="property__type">{item.type}</li>
-              <li className="property__rooms">{item.rooms} Rum</li>
-              <li className="property__size">
-                {item.size} m<span className="super">2</span>
-              </li>
-            </ul>
-          </div>
-        ))}
-      </>
-    );
+  // calling the fetch function
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // if the data is not loaded, we show that to the users
+  if (isLoading) {
+    return <div>Loading</div>;
   }
+
+  // if there is an error fetching data, tell the user
+  if (isError) {
+    return <div>Desværre, der er sket en fejl. Opdater venligst siden.</div>;
+  }
+
+  // if the data is loaded, we make a copy of allProperties to work with further
+  let filteredSortedData = allProperties;
+
+  filterProperties();
+
+  sortProperties();
+
+  return (
+    <>
+      {filteredSortedData.map((item, index) => (
+        <div className="property" key={index}>
+          <img className="property__image" src={item.img} alt={item.title} />
+          <h2 className="property__title">{item.title}</h2>
+          <span className="property__address">
+            {item.address.street}, {item.address.number}, {item.address.zip},{" "}
+            {item.address.city}
+          </span>
+          <ul className="property__info">
+            <li className="property__price">
+              {numeral(item.price).format("0,0")},-
+            </li>
+            <li className="property__type">{item.type}</li>
+            <li className="property__rooms">{item.rooms} Rum</li>
+            <li className="property__size">
+              {item.size} m<span className="super">2</span>
+            </li>
+          </ul>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default CardGrid;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,8 +3,3 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-//ReactDOM.render(
-//  <h1>Hej fra React!</h1>,
-//  document.getElementById('root')
-//);
